a69da09df54060867b98bad18d9b2d7769127132
Jesus Torres <jesustorres38@hotmail.com>
2018-05-14 19:34:50 +0200

    Some styles on desktop navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './_App.scss';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Home   from '../Home/Home';
 import Prices from '../Prices/Prices';
-import Itinerary from '../Itineray/Itinerary';
+import Itinerary from '../Itinerary/Itinerary';
 import Equipment from '../Equipment/Equipment';
 
 
 class App extends Component {
+  
   render() {
+    
     return (
       <div className="App">
-        <Header />
+        <Header {...this.props} />
 
           <Switch>
             <Route exact path="/" component={Home}/>
-            <Route path="/equipment" component={Equipment}/>
-            <Route path="/itinerary" component={Itinerary}/>
-            <Route path="/prices" component={Prices}/>
+            <Route exact path="/equipment" component={Equipment}/>
+            <Route exact path="/itinerary" component={Itinerary}/>
+            <Route exact path="/prices" component={Prices}/>
 
             <Redirect to='/' />
           </Switch>
@@ -31,4 +33,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,30 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
   render() {
+    
+    console.log(this.props);
     return (
       <div className="Header">
         <div className="Navbar_Desktop">
           <div className="Navbar_Logo">
-
+            <img src="/images/logo.jpg" alt="logo-ventum" />
           </div>
 
           <div className="Navbar_Content">
 
             <div className="contacto">
               <a href="tel:+34626482695">Contact: <span>+34 626 482 695</span></a>
-              <a href="mailto:marcabad@ventum.barcelona">Email: marcabad@ventum.barcelona</a>
-              <div className="redes"></div>
+              <a href="mailto:marcabad@ventum.barcelona">Email: <span>marcabad@ventum.barcelona</span></a>
+              <div className="redes">
+                <a href="https://www.facebook.com/ventum.bcn/"><i className="fab fa-facebook-f"></i></a>
+                <a href="https://www.face/"><i className="fab fa-instagram"></i></a>
+              </div>
             </div>
 
             <div className="links">
               <ul>
 
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/equipment">Equipamiento</Link></li>
-                <li><Link to="/itinerary">Itinerario</Link></li>
-                <li><Link to="/prices">Tarifas</Link></li>
-                <li><Link to="/">Contacto</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/equipment">Equipment</Link></li>
+                <li><Link to="/itinerary">Itinerary</Link></li>
+                <li><Link to="/prices">Prices</Link></li>
+                <li><Link to="/">Contact</Link></li>
                 
               </ul>
             </div>
